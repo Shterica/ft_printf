2ee63594d15ecb83145e6c6340dc5d47c440ee83
balamosh <balamosh@balamosh.balamosh>
2021-07-05 13:33:04 -0400

    eval flag funcs proto

---
+++ srcs/ft_eval_flag.c
@@ -0,0 +1,21 @@
+#include "ft_printf.h"
+
+int	ft_flag_dash(t_print *tab, const char *format, int pos)
+{
+	return (pos);
+}
+
+int	ft_flag_zero(t_print *tab, const char *format, int pos)
+{
+	return (pos);
+}
+
+int	ft_flag_point(t_print *tab, const char *format, int pos)
+{
+	return (pos);
+}
+
+int	ft_flag_star(t_print *tab, const char *format, int pos)
+{
+	return (pos);
+}
